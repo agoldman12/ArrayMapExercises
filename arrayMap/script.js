c1d04a2fb28c18b5aaf8c87d00fa73b3bb7ed947
Aaron <agoldman12@gmail.com>
2019-06-15 16:05:04 -0500

    Update script.js
    Added comments for "display answer on page" and added one more function "readyToPutInTheDOM" to the event listener function list.

--- arrayMap/script.js
+++ arrayMap/script.js
@@ -3,6 +3,7 @@ function doubleNumbers(arr){
 	const answer1El = document.getElementById("answer1");
 	const doubles = arr.map(el => el * 2);
 	
+    //display answer on the page
     answer1El.innerHTML === "[" + doubles + "]" ? 
      answer1El.innerHTML = " " 
     :answer1El.innerHTML = "[" + doubles + "]";
@@ -15,6 +16,7 @@ function stringItUp(arr){
   const answer2El = document.getElementById("answer2");
   const strings = arr.map(el => el.toString());
   
+   //display answer on the page
    answer2El.innerHTML === '"' + strings + '"' ? answer2El.innerHTML = "" : answer2El.innerHTML = '"' + strings + '"';
 }
 
@@ -24,10 +26,11 @@ function capitalizeNames(arr){
   const answer3El = document.getElementById("answer3");
   const cap = arr.map(el => '"' + el[0].toUpperCase() + el.slice(1).toLowerCase() + '"');
 
+  //display answer on the page
   answer3El.innerHTML === "[" + cap + "]" ? answer3El.innerHTML = "" : answer3El.innerHTML = "[" + cap + "]";
 }
 
-
+//Raw Data
 const people = [
     {
         name: "Trey Anastasio",
@@ -61,7 +64,7 @@ function namesOnly(people){
   const answer4El = document.getElementById("answer4");
 	const person = people.map(person => '"' + person.name + '"');
   
-  
+  //display answer on the page
   answer4El.innerHTML === "[" + person + "]" ?
     answer4El.innerHTML = " "
    :answer4El.innerHTML = "[" + person + "]";
@@ -72,14 +75,19 @@ function namesOnly(people){
 function matrixNames(people){
 	  const answer5El = document.getElementById("answer5");
     const matrix = people.map(person => person.age > 20 ? person.name + " can go to the Matrix"  :  person.name + "  is underage!!");
-
+    
+    //display answer on the page
     answer5El.innerHTML === '"' + matrix + '"' ? answer5El.innerHTML = "" : answer5El.innerHTML = '"' + matrix + '"';
 }
 
 
 //6) Make an array of the names in h1s, and the ages in h2s
 function readyToPutInTheDOM(people){
-  return people.map(person => "<h1>" + person.name + "</h1>" + "<h2>" + person.age + "</h2>");
+  const answer6El = document.getElementById("answer6");
+  const h1h2 = people.map(person => "<h1>" + person.name + "</h1>" + "<h2>" + person.age + "</h2>");
+  
+  //display answer on the page
+  answer6El.innerHTML === '"' + h1h2 + '"' ? answer6El.innerHTML = " " : answer6El.innerHTML = '"' + h1h2 + '"';
 }
 
 
@@ -95,20 +103,21 @@ function initButtonListeners(){
         namesOnly(people);
       }else if(e.target.classList.contains('btn5')){
         matrixNames(people);
+      }else if(e.target.classList.contains('btn6')){
+        readyToPutInTheDOM(people);
       }
 
-      //toggle button display
-      const selectedBtnId = e.target.id;
-      const btns = document.getElementsByTagName("input");
-      for(i = 0; i < btns.length; i++){
-          if(selectedBtnId === btns[i].id){
-            btns[i].value === "Show Answer" ? btns[i].value = "Hide Answer" 
-            : btns[i].value = "Show Answer";
-          }
-      }
+        //toggle button display
+        const selectedBtnId = e.target.id;
+        const btns = document.getElementsByTagName("input");
+        for(i = 0; i < btns.length; i++){
+            if(selectedBtnId === btns[i].id){
+              btns[i].value === "Show Answer" ? btns[i].value = "Hide Answer" 
+              : btns[i].value = "Show Answer";
+            }
+        }
       
   });
- 
 }
 
 
